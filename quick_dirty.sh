5fc26a13220015499da2dbd1a05803bdad190e20
Alejandro Baez <Alejan.Baez@gmail.com>
2014-01-05 15:51:56 -0500

    begin addition of redmine

--- quick_dirty.sh
+++ quick_dirty.sh
@@ -10,7 +10,9 @@ if [ $(cat whoami ) -eq "root" ]; then
   echo "need to run as root."
   exit 2
 else
-  mkdir ~/tmp && cd ~/tmp;
+  mkdir ~/tmp
+  cp ./ale-rhodecode.sh ~/tmp/ale-rhodecode.sh
+  cd ~/tmp
   
   dependencies
   rabbitmq_install
@@ -22,21 +24,30 @@ fi
 
 dependencies_install() {
   yum -y update; 
-  yum -y install vim ntp openssh-clients wget gcc make python-devel
+  yum -y install vim ntp openssh-clients wget gcc make python-devel sqlite
   
   # installs epel 6.8 repo
   rpm -Uvh http://mirror.itc.virginia.edu/fedora-epel/6/i386/epel-release-6-8.noarch.rpm
   
   yum -y update
-  yum -y install erlang openldap openldap-clients openldap-devel openssl-devel 
+  yum -y install erlang openldap openldap-clients openldap-devel openssl-devel ruby rubygems rubygem-passenger rubygem-rake ruby-rdoc
 }
 
 
 rabbitmq_install() {
   rpm -Uvh http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.2/rabbitmq-server-3.2.2-1.noarch.rpm
+  
+  chkconfig --add rabbitmq-server
+  chkconfig --level 345 rabbitmq-server on
+  
+  # run rabbitmq for settings
+  service rabbitmq-server start
+
   rabbitmqctl add_user rhodeuser rhodepass
   rabbitmqctl add_vhost rhodevhost
   rabbitmqctl set_permissions -p rhodevhost rhodeuser ".*" ".*" ".*" 
+  
+  service rabbitmq-server stop
 }
 
 
@@ -86,9 +97,18 @@ rhodecode_boot() {
   chown -R rhodecode:rhodecode $RHODE_ROOT /var/log/rhodecode /var/run/rhodecode
   cd ~/tmp
   # tmp location of gist for init
-  wget  "https://gist.github.com/abaez/89bbda3d46c8b681cea1/raw/88021509003cf3779d518327ab3785d23df4a338/ale-rhodecode.sh"
-  chmod +rwx ./ale-rhodecode.sh
+  chmod +rwx ~/tmp/ale-rhodecode.sh
   
   cp ./ale-rhodecode.sh /etc/init.d/rhodecode
+  chkconfig --add rhodecode
+  chkconfig rhodecode on
+}
+
+redmine_install() {
+  cd ~/tmp
+  curl http://rubyforge.org/frs/download.php/77242/redmine-2.4.0.tar.gz
+  tar xvfz ./redmine*gz
   
+  mkdir /var/www/redmine
+  cp -av ./redmine-2.4.0 /var/www/redmine
 }
